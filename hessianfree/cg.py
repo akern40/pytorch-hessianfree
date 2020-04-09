72635f21f039a3f2f55ab2e27917832124602f0d
Adam Kern <adam.kern@wustl.edu>
2020-04-09 17:16:51 -0400

    First stab at preconditioned CG

---
+++ hessianfree/cg.py
@@ -0,0 +1,52 @@
+from typing import Callable, Union
+
+import torch
+
+_preconditioners = {"diag": torch.diagonal}
+
+
+def pcg(
+    A: torch.Tensor,
+    b: torch.Tensor,
+    x0: Union[torch.Tensor, None] = None,
+    preconditioner: str = "diag",  # A pre-defined preconditioner function
+    max_iter: int = None,
+    err_tol: float = 1e-6,
+):
+    """Compute the solution to Ax=b using the preconditioned conjugate gradient method"""
+    if x0 is None:
+        x0 = torch.zeros_like(b)
+    if max_iter is None:
+        max_iter = A.shape[0]
+
+    x = x0
+    count = 0
+    residual = b - A @ x
+
+    P = _preconditioners[preconditioner](A)
+    P_inv = torch.inverse(P)
+    direction = P_inv @ residual
+
+    delta0 = residual.transpose(0, 1) @ direction
+    delta_new = delta0
+
+    while count < max_iter and delta_new / delta0 > (err_tol ** 2):
+        q = A @ direction
+        alpha = delta_new / (direction.transpose(0, 1) @ q)
+        x += alpha * direction
+
+        if count % 50 == 0:
+            residual = b - A @ x
+        else:
+            residual -= alpha * q
+
+        s = P_inv @ residual
+        delta_old = delta_new
+        delta_new = residual.transpose(0, 1) @ s
+
+        beta = delta_new / delta_old
+        direction = s + beta * direction
+
+        count += 1
+
+    return x
